d51e07441f0f480f2fd838f3baebc4a266dcc35b
Hack Kiếm Sĩ <84692657+phanxuanquang@users.noreply.github.com>
2023-12-10 17:06:59 +0700

    Add copilot

--- bing-chat-api/app.py
+++ bing-chat-api/app.py
@@ -1,12 +1,19 @@
+from flask import Flask, jsonify, request
 import asyncio
 from sydney import SydneyClient
 
-question = "hello"
+app = Flask(__name__)
 
-async def main():
+async def ask_sydney(question):
     async with SydneyClient() as sydney:
-        response = await sydney.ask(question)
-        print(response)
+        response, recommended_questions = await sydney.ask(question, suggestions=True ,raw=False)
+        return response, recommended_questions
+
+@app.route('/ask', methods=['GET'])
+def ask():
+    question = request.args.get('question', default = "Hello", type = str)
+    response, recommended_questions = asyncio.run(ask_sydney(question))
+    return jsonify({'response': response, 'recommended_questions': recommended_questions})
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    app.run(port=5000)
