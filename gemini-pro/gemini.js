4d7b656c87be35dc7a164bab001c8cd4bc281a3f
Quang Phan Xuan <phanxuanquang2@gmail.com>
2023-12-18 18:22:06 +0700

    Fix

--- gemini-pro/gemini.js
+++ gemini-pro/gemini.js
@@ -9,33 +9,23 @@ app.use(cors());
 const genAI = new GoogleGenerativeAI(atob('QUl6YVN5RFR4dkpFZEhNRzVhOGI5ejhTQ3V1czRqZ25MOTFfeWk0'));
 const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
 
-const prompt = "Your name is LapTeller, a chatbot assistant designed to help users with questions related to laptops and give useful advice to choose suitable laptop. You are only allowed to answer questions related to laptops. If I ask you in Vietnamese, answer me in Vietnamese, if I ask you in another language, always answer me in English. Your answer must be short and easy to understand for non-tech people. You can ask me in return to clarify the question. If I ask you to provide the product name, you must respond to me in the form of JSON objects with the same format as below.{   \"products\": [     {       \"name\": \"Laptop Name\",       \"screenSize\": 15.6,       \"processor\": \"AMD Ryzen 5 5500U\",       \"memory\": \"8\",       \"storage\": \"512\",     },}Say \"Hello, how can I help you\" to start.";
-//const prompt = "Hello";
+//const prompt = "Your name is LapTeller, a chatbot assistant designed to help users with questions related to laptops and give useful advice to choose suitable laptop. You are only allowed to answer questions related to laptops. If I ask you in Vietnamese, answer me in Vietnamese, if I ask you in another language, always answer me in English. Your answer must be short and easy to understand for non-tech people. You can ask me in return to clarify the question. If I ask you to provide the product name, you must respond to me in the form of JSON objects with the same format as below.{   \"products\": [     {       \"name\": \"Laptop Name\",       \"screenSize\": 15.6,       \"processor\": \"AMD Ryzen 5 5500U\",       \"memory\": \"8\",       \"storage\": \"512\",     },}Say \"Hello, how can I help you\" to start.";
 
-function removeMarkdownFrom(text) {
-  // Remove bold formatting: **bold**
-  text = text.replace(/\*\*(.*?)\*\*/g, '$1');
-
-  // Remove italic formatting: *italic*
-  text = text.replace(/\*(.*?)\*/g, '$1');
-
-  // Remove inline code formatting: `code`
-  text = text.replace(/`(.*?)`/g, '$1');
-
-  // Remove headers: # Header
-  text = text.replace(/^#+\s*(.*)/gm, '$1');
-
-  // Remove unordered list: - item
-  text = text.replace(/^\s*-\s*(.*)/gm, '$1');
-
-  // Remove ordered list: 1. item
-  text = text.replace(/^\s*\d+\.\s*(.*)/gm, '$1');
-
-  // Remove blockquote: > quote
-  text = text.replace(/^\s*>\s*(.*)/gm, '$1');
-
-  return text;
+async function getFileContentFromURL(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Network response was not ok.');
+    }
+    const text = await response.text();
+    return text;
+  } catch (error) {
+    console.error('Error:', error);
+    return "Hello";
+  }
 }
+
+var prompt = getFileContentFromURL('https://raw.githubusercontent.com/phanxuanquang/LapTeller-Backend/main/gemini-pro/promt.txt');
 
 app.post('/ask', async (req, res) => {
   try {
@@ -45,7 +35,7 @@ app.post('/ask', async (req, res) => {
       history: [
         {
           role: 'user',
-          parts: prompt,
+          parts: await prompt,
         },
         {
           role: 'model',
@@ -61,7 +51,7 @@ app.post('/ask', async (req, res) => {
     const response = await result.response;
     const text = response.text();
     console.log(response.text());
-    res.json({ answer: removeMarkdownFrom(text) });
+    res.json({ answer: text });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
