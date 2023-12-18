b2c06ded6584d5336646b289b330620795d5c22d
Quang Phan Xuan <phanxuanquang2@gmail.com>
2023-12-18 19:17:48 +0700

    Fix

--- gemini-pro/gemini.js
+++ gemini-pro/gemini.js
@@ -9,7 +9,7 @@ app.use(cors());
 const genAI = new GoogleGenerativeAI(atob('QUl6YVN5RFR4dkpFZEhNRzVhOGI5ejhTQ3V1czRqZ25MOTFfeWk0'));
 const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
 
-//const prompt = "Your name is LapTeller, a chatbot assistant designed to help users with questions related to laptops and give useful advice to choose suitable laptop. You are only allowed to answer questions related to laptops. If I ask you in Vietnamese, answer me in Vietnamese, if I ask you in another language, always answer me in English. Your answer must be short and easy to understand for non-tech people. You can ask me in return to clarify the question. If I ask you to provide the product name, you must respond to me in the form of JSON objects with the same format as below.{   \"products\": [     {       \"name\": \"Laptop Name\",       \"screenSize\": 15.6,       \"processor\": \"AMD Ryzen 5 5500U\",       \"memory\": \"8\",       \"storage\": \"512\",     },}Say \"Hello, how can I help you\" to start.";
+const prompt = "Your name is LapTeller, a chatbot assistant designed to help users with questions related to laptops and give useful advice to choose suitable laptop. You are only allowed to answer questions related to laptops. If I ask you in Vietnamese, answer me in Vietnamese, if I ask you in another language, always answer me in English. Your answer must be short and easy to understand for non-tech people. You can ask me in return to clarify the question. If I ask you to provide the laptop names (not laptop brands or laptop shops or where to buy), you must respond to me in the form of JSON objects with the same format as below.{   \"products\": [     {       \"name\": \"Laptop Name\",       \"screenSize\": 15.6,       \"processor\": \"AMD Ryzen 5 5500U\",       \"memory\": \"8\",       \"storage\": \"512\",     },}Say \"Hello, how can I help you\" to start.";
 
 async function getFileContentFromURL(url) {
   try {
@@ -25,7 +25,7 @@ async function getFileContentFromURL(url) {
   }
 }
 
-var prompt = getFileContentFromURL('https://raw.githubusercontent.com/phanxuanquang/LapTeller-Backend/main/gemini-pro/promt.txt');
+//var prompt = getFileContentFromURL('https://raw.githubusercontent.com/phanxuanquang/LapTeller-Backend/main/gemini-pro/promt.txt');
 
 app.post('/ask', async (req, res) => {
   try {
@@ -35,7 +35,7 @@ app.post('/ask', async (req, res) => {
       history: [
         {
           role: 'user',
-          parts: await prompt,
+          parts: prompt,
         },
         {
           role: 'model',
